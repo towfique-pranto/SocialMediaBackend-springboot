3f6ce35d861d759d33b2ae6c333cfa2ab4080005
Towfique Kabir <towfique.pranto@gmail.com>
2026-05-13 22:12:12 +0600

    feat:connect project to database

--- src/main/java/com/example/SocialMedia/controller/UserController.java
+++ src/main/java/com/example/SocialMedia/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.example.SocialMedia.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class UserController {
 }
