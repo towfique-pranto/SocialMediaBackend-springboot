99ba3ce5b31e7a553f6b20d4d81d05af871e5da1
Towfique Kabir <towfique.pranto@gmail.com>
2026-05-13 22:26:42 +0600

    feat:create entity properties: id (Long), fullName (String), email (String), and bio (String).

--- src/main/java/com/example/SocialMedia/entity/UserEntity.java
+++ src/main/java/com/example/SocialMedia/entity/UserEntity.java
@@ -1,4 +1,19 @@
 package com.example.SocialMedia.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String bio;
 }
